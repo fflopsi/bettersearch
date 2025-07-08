86a7ce395bf5c93e61ceff45ba02643a31d70e6d
fflopsi <florian.l.frauenfelder@gmail.com>
2025-07-08 16:29:30 +0200

    Bump compileSdk and targetSdk to 36 for Android 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
   namespace = "ch.frauenfelderflorian.bettersearch"
-  compileSdk = 35
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "ch.frauenfelderflorian.bettersearch"
     minSdk = 26
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 1
     versionName = libs.versions.app.get()
 
