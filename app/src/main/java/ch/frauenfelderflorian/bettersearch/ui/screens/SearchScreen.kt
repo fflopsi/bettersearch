12f23341677ab34ecb58613fe2ccc07f15333a0c
fflopsi <florian.l.frauenfelder@gmail.com>
2025-07-08 18:39:24 +0200

    Fix suggestions column size

--- app/src/main/java/ch/frauenfelderflorian/bettersearch/ui/screens/SearchScreen.kt
+++ app/src/main/java/ch/frauenfelderflorian/bettersearch/ui/screens/SearchScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -141,7 +142,7 @@ fun SearchScreen(
           .padding(16.dp)
           .fillMaxWidth(),
       )
-      LazyColumn(modifier = Modifier.fillMaxWidth()) {
+      LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(
           items = historySuggestions,
           key = { it.time },
